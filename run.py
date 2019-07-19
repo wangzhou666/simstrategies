62246e36b1708ca1ac13a55b6b27647e3bd94e72
wangzhou666 <wdx6666666@gmail.com>
2019-07-18 23:06:19 -0700

    Support generic type of strategies.

--- run.py
+++ run.py
@@ -6,8 +6,11 @@ import quoteapi
 import simulator
 import strategy
 
+_STRATEGIES = {
+    'scaledetf': strategy.ScaledETFStrategy,
+}
+
 def main():
-  #TODO: support customized strategies
   parser = ArgumentParser(description='Run the simulator.')
   parser.add_argument('--config', '-c', dest='configs',
                       action='append', default=[])
@@ -24,14 +27,20 @@ def run_with_config(config_filename):
       apikey=config['alphavantage_key'])
   acc = account.VirtualAccount(
       cash=config['account']['cash'])
-  stg = strategy.ScaledETFStrategy(
-      symbol=config['strategy']['symbol'],
-      amplifier=config['strategy']['scale'],
-      allow_float_position=config['strategy'].get('allow_float_position'))
-  hst = {'daily_adjusted': av_client.load_daily_adjusted(symbol=config['strategy']['symbol'])}
+  
+  stg_name = config['strategy']['name']
+  stg_kwargs = config['strategy']['kwargs']
+  stg_cls = _STRATEGIES[stg_name]
+  stg = stg_cls(**stg_kwargs)
+
+  hst = {
+      'daily_adjusted': av_client.load_daily_adjusted(
+          symbol=config['history']['symbol'])
+  }
   sim = simulator.DailyStrategySimulator(
       account=acc, strategy=stg, history=hst,
-      start=config['date']['start'], end=config['date']['end'])
+      start=config['history']['start'],
+      end=config['history']['end'])
 
   sim.simulate()
   print(sim)
